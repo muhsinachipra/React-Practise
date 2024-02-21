5d6e3287b2986cc446a91f708b394527275c558b
muhsinachipra <muhsinachipra@gmail.com>
2024-02-21 21:46:28 +0530

    first commit

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer (){
+    return (
+        <footer>
+            <p>&copy; {new Date().getFullYear()} My Site</p>
+        </footer>
+    );
+}
+
+export default Footer
---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+
+function Header (){
+
+    return (
+        <header>
+            <h1>My Website</h1>
+            <nav>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Service</a></li>
+                <li><a href="#">Contact</a></li>
+            </nav>
+            <hr />
+        </header>
+    );
+}
+
+export default Header
