60177d09fa42e1429f5111b26fc6f0eed79dc363
muhsinachipra <muhsinachipra@gmail.com>
2024-02-23 20:21:16 +0530

    EVENT HANDLING

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,25 @@ import UserGreeting from "./UserGreeting";
 import List from "./List";
 
 function App() {
-  const fruits = [
-    { id: 1, name: "apple", calories: 95 },
-    { id: 2, name: "orange", calories: 45 },
-    { id: 3, name: "banana", calories: 105 },
-    { id: 4, name: "coconut", calories: 159 },
-    { id: 5, name: "pineapple", calories: 37 },
-  ];
+  // const fruits = [
+  //   { id: 1, name: "apple", calories: 95 },
+  //   { id: 2, name: "orange", calories: 45 },
+  //   { id: 3, name: "banana", calories: 105 },
+  //   { id: 4, name: "coconut", calories: 159 },
+  //   { id: 5, name: "pineapple", calories: 37 },
+  // ];
 
-  const vegatables = [
-    { id: 6, name: "potato", calories: 110 },
-    { id: 7, name: "carrot", calories: 15 },
-    { id: 8, name: "coliflower", calories: 25 },
-    { id: 9, name: "gergeel", calories: 63 },
-    { id: 10, name: "cabbage", calories: 50 },
-  ];
+  // const vegatables = [
+  //   { id: 6, name: "potato", calories: 110 },
+  //   { id: 7, name: "carrot", calories: 15 },
+  //   { id: 8, name: "coliflower", calories: 25 },
+  //   { id: 9, name: "gergeel", calories: 63 },
+  //   { id: 10, name: "cabbage", calories: 50 },
+  // ];
 
   return (
     <>
-      {fruits.length > 0 && <List  items={fruits} category="Fruits" />}
-      {vegatables.length > 0 && <List items={vegatables} category="Vegatables" />}
+      <Button/>
     </>
   );
 }
--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -1,5 +1,22 @@
-import styles from './Button.module.css'
+import styles from "./Button.module.css";
 
-export default function Button(){
-    return (<button className={styles.button}>Click Me</button>);
+export default function Button() {
+  //   const handleClick = () => console.log("OUCH!");
+  //   const handleClick2 = (name) => console.log(`${name} stop clicking me`);
+  let count = 0
+
+  const handleClick = (name) => {
+    if(count<3){
+        count++
+        console.log(`${name} you clicked me ${count} times`)
+    }else{
+        console.log(`${name} stop clicking me`)
+    }
+  };
+
+  return (
+    <button className={styles.button} onClick={() => handleClick("MUHSIN")}>
+      Click Me 😀
+    </button>
+  );
 }
