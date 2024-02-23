b3f33e86e906f0fd67a788128894c59b6f163055
muhsinachipra <muhsinachipra@gmail.com>
2024-02-23 21:20:38 +0530

    useState Hook

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,12 @@ import Student from "./Student";
 import UserGreeting from "./UserGreeting";
 import List from "./List";
 import ProfilePicture from "./ProfilePicture";
+import MyComponent from "./MyComponent";
 
 function App() {
-  // const fruits = [
-  //   { id: 1, name: "apple", calories: 95 },
-  //   { id: 2, name: "orange", calories: 45 },
-  //   { id: 3, name: "banana", calories: 105 },
-  //   { id: 4, name: "coconut", calories: 159 },
-  //   { id: 5, name: "pineapple", calories: 37 },
-  // ];
-
-  // const vegatables = [
-  //   { id: 6, name: "potato", calories: 110 },
-  //   { id: 7, name: "carrot", calories: 15 },
-  //   { id: 8, name: "coliflower", calories: 25 },
-  //   { id: 9, name: "gergeel", calories: 63 },
-  //   { id: 10, name: "cabbage", calories: 50 },
-  // ];
 
   return (
-    <>
-      <ProfilePicture/>
-    </>
+    <MyComponent/>
   );
 }
 
---
+++ src/MyComponent.jsx
@@ -0,0 +1,28 @@
+import React, {useState} from "react";
+
+export default function MyComponent(){
+    let [name, setName] = useState("Guest");
+    let [age, setAge] = useState(0);
+    let [dump, setDump] = useState(false);
+
+    const updateName = () => {
+        setName('muhsin')
+    } 
+    const IncreamentAge = () => {
+        setAge(++age)
+    } 
+    const toggleDump = () => {
+        setDump(!dump)
+    }
+
+    return (
+        <div>
+            <p>Name: {name}</p>
+            <button onClick= {updateName}>Set Name</button>
+            <p>Age: {age}</p>
+            <button onClick= {IncreamentAge}>Increament Age</button>
+            <p>is dump: {dump?"Yes":"Nope"}</p>
+            <button onClick= {toggleDump}>toggle dump</button>
+        </div>
+    );
+}
